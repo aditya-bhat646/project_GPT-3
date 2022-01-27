1631e53102065e45bd347845d1b311ec832f0e9c
aditya-bhat646 <adityabhat646@gmail.com>
2022-01-27 11:58:57 +0530

    added alt property to img tag

--- src/Containers/header/Header.jsx
+++ src/Containers/header/Header.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import './header.css';
-import people from '../../Assets/people.png';
-import ai from '../../Assets/ai.png';
+import React from "react";
+import "./header.css";
+import people from "../../Assets/people.png";
+import ai from "../../Assets/ai.png";
 
 const Header = () => {
   return (
@@ -11,27 +11,22 @@ const Header = () => {
           Let's Build Something amazing with GPT-3 OpenAI
         </h1>
         <p>
-          Yet bed any for travelling assistance indulgence
-          unpleasing. Not thoughts all exercise blessing.
-          Indulgence way everything joy alteration boisterous the
-          attachment. Party we years to order allow asked of.
+          Yet bed any for travelling assistance indulgence unpleasing. Not
+          thoughts all exercise blessing. Indulgence way everything joy
+          alteration boisterous the attachment. Party we years to order allow
+          asked of.
         </p>
         <div className="gpt3__header-content__input">
-          <input
-            type="email"
-            placeholder="Your Email Address"
-          ></input>
+          <input type="email" placeholder="Your Email Address"></input>
           <button type="button">Get Started</button>
         </div>
         <div className="gpt3__header-content_people">
-          <img src={people} alt='people'></img>
-          <p>
-            1600 people requested access a visit in last 24 hours
-          </p>
+          <img src={people} alt="people"></img>
+          <p>1600 people requested access a visit in last 24 hours</p>
         </div>
       </div>
       <div className="gpt3__header-image">
-        <img src={ai}></img>
+        <img src={ai} alt="ai"></img>
       </div>
     </div>
   );
